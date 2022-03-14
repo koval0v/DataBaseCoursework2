43b5101cdaecbbc78d894b2b955c1393aac9be31
Koval Vadym <vzlobinkov@gmail.com>
2022-03-14 13:53:29 +0200

    added template 2

---
+++ PlantGrowing_CourseWork_2course/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlantGrowing_CourseWork_2course.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
